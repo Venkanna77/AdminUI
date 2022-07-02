5296f4e45d5ccde38eb333ea1b7d6c194ba8a3e6
venkanna77 <venkanna948@gmail.com>
2022-07-02 22:07:30 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+    )
+      .then((response) => response.json())
+      .then((data) => setUserData(data))
+      .catch((error) => console.log(error.message));
+
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Admin UI</h1>
+      <input className="search" type="text" />
+      <table className="table">
+        <tbody>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+          {userData.map((user) => (
+            <tr className="border_bottom " key={user.id}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td><input type="text" className="input" value={user.name} disabled="true"/></td>
+              <td>{user.email}</td>
+              <td>{user.role}</td>
+              <td>edit</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p>page</p>
     </div>
   );
-}
+};
 
 export default App;
